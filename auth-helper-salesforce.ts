0d4e7d110a7600ea5b9d2132f03380d9e5525cc4
Daniel Rice <drice304@gmail.com>
2017-10-17 12:37:09 -0600

    indent 2 spaces, use _logout and revoke as separate method

--- auth-helper-salesforce.ts
+++ auth-helper-salesforce.ts
@@ -13,55 +13,64 @@ import * as TnsOAuth from './tns-oauth-interfaces';
 export class AuthHelperSalesforce extends AuthHelper implements TnsOAuth.ITnsAuthHelper {
   //Constructs the the object with specified id, secret and scope
   constructor(
-      authority: string,
-      clientId: string,
-      redirectUri: string,
-      responseType: string,
-      scope: Array<string>,
-      clientSecret?: string,
+    authority: string,
+    clientId: string,
+    redirectUri: string,
+    responseType: string,
+    scope: Array<string>,
+    clientSecret?: string,
   ) {
     super();
     var scopeStr = scope.join('%20');
     this.credentials = {
-        authority: authority,
-        authorizeEndpoint: '/services/oauth2/authorize',
-        tokenEndpoint: '/services/oauth2/token',
-        revokeEndpoint: '/services/oauth2/revoke',
-        clientId: clientId,
-        clientSecret: clientSecret,
-        redirectUri: redirectUri,
-        responseType: responseType,
-        scope: scopeStr
+      authority: authority,
+      authorizeEndpoint: '/services/oauth2/authorize',
+      tokenEndpoint: '/services/oauth2/token',
+      revokeEndpoint: '/services/oauth2/revoke',
+      clientId: clientId,
+      clientSecret: clientSecret,
+      redirectUri: redirectUri,
+      responseType: responseType,
+      scope: scopeStr
     };
   }
-  //Gets cookie domains for logging out
-  public logout(): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-        try {
-            // call revoke token here 
-            var revoke_url = this.credentials.authority + this.credentials.revokeEndpoint;
-            var post_headers = {
-                // XXX: Not sure if this header is needed
-                //'Host': URL.parse(revoke_url, true),
-                'Content-Type': 'application/x-www-form-urlencoded'
-            };
-            var post_body = querystring.stringify({token: this.tokenResult.refreshToken});
 
-            http.request({
-                url: revoke_url,
-                method: 'POST',
-                headers: post_headers,
-                content: post_body
-            }).then((response: http.HttpResponse) => {
-                if (response.statusCode != 200) {
-                    throw new Error(`Failed logout with status ${ response.statusCode }.`);
-                }
-            });
-            this.tokenResult = null;
-            resolve();
-        } catch (er) {
-            reject(er);
-        }
+  //Gets cookie domains for logging out
+  public logout(successPage?: string, cookieDomains?: Array<string>): Promise<void> {
+    if (!cookieDomains) {
+      cookieDomains = [".salesforce.com", ".force.com"];
+    }
+    return this._logout(successPage, cookieDomains);
+  }
+
+  // revokes token using Salesforce APIs (doesn't remove webview cookies)
+  public revoke(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      try {
+        // call revoke token here 
+        var revoke_url = this.credentials.authority + this.credentials.revokeEndpoint;
+        var post_headers = {
+          // XXX: Not sure if this header is needed
+          //'Host': URL.parse(revoke_url, true),
+          'Content-Type': 'application/x-www-form-urlencoded'
+        };
+        var post_body = querystring.stringify({ token: this.tokenResult.refreshToken });
+
+        http.request({
+          url: revoke_url,
+          method: 'POST',
+          headers: post_headers,
+          content: post_body
+        }).then((response: http.HttpResponse) => {
+          if (response.statusCode != 200) {
+            throw new Error(`Failed logout with status ${response.statusCode}.`);
+          }
+        });
+        this.tokenResult = null;
+        resolve();
+      } catch (er) {
+        reject(er);
+      }
     });
 
   }
